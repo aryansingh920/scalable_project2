35568996de8b71777b014a95adb95f6ee284bcde
Aryan Singh <aryansingh920@outlook.com>
2024-10-12 23:33:43 +0100

    commands

--- src/image_processing/run.sh
+++ src/image_processing/run.sh
@@ -8,12 +8,12 @@ pip3 install -r requriements.txt
 
 
 # to generate
-python src/image_processing/generate.py --width 128 --height 64 --length 4 --symbols symbols.txt --count 3200 --output-dir test
+python src/image_processing/generate.py --width 128 --height 64 --length 4 --symbols src/image_processing/symbols.txt --count 3200 --output-dir src/image_processing/test
 
 
 # to train
-python train.py --width 128 --height 64 --length 4 --symbols symbols.txt --batch-size 32 --epochs 5 --output-model test.h5 --train-dataset training_data --validate-dataset validation_data
+python src/image_processing/train.py --width 128 --height 64 --length 4 --symbols src/image_processing/symbols.txt --batch-size 32 --epochs 5 --output-model src/image_processing/test.h5 --train-dataset src/image_processing/training_data --validate-dataset src/image_processing/validation_data
 
 
 # to predict 
-python classify.py --model-name test --captcha-dir validation_data --output output.txt --symbols symbols.txt
+python src/image_processing/classify.py --model-name src/image_processing/test --captcha-dir src/image_processing/validation_data --output src/image_processing/output.txt --symbols src/image_processing/symbols.txt
