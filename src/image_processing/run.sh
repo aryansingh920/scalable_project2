748b1fe7d06166b40b851675be546909dc6a6d91
Aryan Singh <aryansingh920@outlook.com>
2024-10-25 21:57:54 +0100

    refactor structure 25/10/2024 13:55

--- src/image_processing/run.sh
+++ src/image_processing/run.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 #run.sh
 
-python3.10 -m venv venv
+python3 -m venv venv
 source ./venv/bin/activate
 
-pip3.10 install -r requirements.txt
+pip3 install -r requirements.txt
 
 #to download all files from csv
-python3.10 src/access_files/download_files.py
+python3 src/access_files/download_files.py
 
 #to compare and get a csv files not downloaded to download the files again
-python3.10 src/access_files/compareCsv.py
+python3 src/access_files/compareCsv.py
 
 # to generate
-python3.10 src/image_processing/generate_process/generate.py --width 192 --height 96 --length 6 --symbols src/image_processing/symbols.txt --count 3200 --output-dir src/data/test --font src/image_processing/font/font1.ttf
+python3 src/image_processing/generate_process/generate.py --width 192 --height 96 --length 6 --symbols src/image_processing/symbols.txt --count 3200 --output-dir src/data/test --font src/image_processing/font/font1.ttf
 
 
 
 #to divide to training data and validation_data
-python3.10 src/image_processing/generate_process/divide.py
+python3 src/image_processing/generate_process/divide.py
 
 
 # to train
-python3.10 src/image_processing/train/train.py --train-dataset "src/data/Aryan/captchas_test" --validate-dataset "src/data/Vishalini/captchas_test" --batch-size 64 --epochs 12 --output-model-name "src/image_processing/captcha_model.keras" --length 6 --width 192 --height 96 --symbols "src/image_processing/symbols.txt"
+python3 src/image_processing/train/train.py --train-dataset "src/data/Aryan/captchas_test" --validate-dataset "src/data/Vishalini/captchas_test" --batch-size 64 --epochs 12 --output-model-name "src/image_processing/captcha_model.keras" --length 6 --width 192 --height 96 --symbols "src/image_processing/symbols.txt"
 
 
 
 # to predict 
-python3.10 src/image_processing/classify/classify.py --width 192 --height 96 --length 6 --captcha-dir src/data/Aryan/captchas_test --input-model src/image_processing/captcha_model.keras.keras --symbols src/image_processing/symbols.txt --output src/image_processing/output/output.txt
+python3 src/image_processing/classify/classify.py --width 192 --height 96 --length 6 --captcha-dir src/data/Aryan/captchas_test --input-model src/image_processing/captcha_model.keras.keras --symbols src/image_processing/symbols.txt --output src/image_processing/output/output.txt
 
 
 
 #to convert txt to csv
-python3.10 src/image_processing/generate_process/convert_to_csv.py
+python3 src/image_processing/generate_process/convert_to_csv.py
